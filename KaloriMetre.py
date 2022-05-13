d4e20caf853a89e5b2acf3293e32e32d0028957f
Sungu Erdem <sunguerd@gmail.com>
2022-05-13 20:35:11 +0300

    Add files via upload

---
+++ KaloriMetre.py
@@ -0,0 +1,185 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+class Kalori:
+    def __init__(self):
+        self.son2 = son2
+        self.son3 = son3
+    def HangCip():
+        secim = input("Hangi Cipsi seçmek istiyorsunuz: (1-Pringles 2-) Ruffles 3-)Pacha Cips 4-) Lays 5-) Cipso 6-) Vegeat's 7-) Doritos 8-) Çerezza ")
+        if secim == '1':
+            p = 'Pringles'
+            print(p,"Yağ oranı günlük alınması gereken yağ oranının üstündedir")
+            print(p,"Doymuş Yağ oranı günlük alınması gereken yağ oranının altındadır.")
+            print(p,"Günlük tüketilmesi gereken karhonhidrat bakımından çok üstündedir")
+            print(p,"Günlük tüketilmesi gereken protein bakımından yeterli.")
+            print(p,"Günlük tüketilmesi gereken lif bakımından yeterli.")
+            print(p,"Günlük tüketilmesi gereken tuz bakımından üstündedir")
+            print(p,"İçerisinde kolajen bulunmuyor.")
+            print(p,"İçerisinde trans yağ bulunmuyor.")
+            print(p,"İçerisinde palm yağ bulunuyor.")
+            print(p,"İçerisinde şeker bulunmuyor.")
+        elif secim == '2':
+            p = 'Ruffles'
+            print(p,"Yağ oranı günlük alınması gereken yağ oranının çok üstündedir")
+            print(p,"Doymuş Yağ oranı günlük alınması gereken yağ oranının altındadır.")
+            print(p,"Günlük tüketilmesi gereken karhonhidrat bakımından çok üstündedir")
+            print(p,"Günlük tüketilmesi gereken protein bakımından yeterli.")
+            print(p,"Günlük tüketilmesi gereken lif bakımından üstündedir")
+            print(p,"Günlük tüketilmesi gereken tuz bakımından üstündedir")
+            print(p,"İçerisinde kolajen bulunmuyor.")
+            print(p,"İçerisinde trans yağ bulunmuyor.")
+            print(p,"İçerisinde palm yağ bulunuyor.")
+            print(p,"Günlük alınması gereken şeker miktarından fazla.")
+        elif secim == '3':
+            p = 'Pacha Cips'
+            print(p,"Yağ oranı günlük alınması gereken yağ oranının çok üstündedir")
+            print(p,"Doymuş Yağ oranı günlük alınması gereken yağ oranının altındadır.")
+            print(p,"Günlük tüketilmesi gereken karhonhidrat bakımından yeterli.")
+            print(p,"Günlük tüketilmesi gereken protein bakımından çok üstündedir")
+            print(p,"DİKKAT! İçerisindeki lif miktarı bilinmiyor")
+            print(p,"Günlük tüketilmesi gereken tuz bakımından üstündedir")
+            print(p,"İçerisindeki kolajen miktarı günlük alınması gerekenden oldukça yüksekte.")
+            print(p,"İçerisinde trans yağ bulunmuyor.")
+            print(p,"İçerisinde palm yağ bulunmuyor.")
+            print(p,"İçerisinde şeker bulunmuyor.")
+        elif secim == '4':
+            p = 'Lays'
+            print(p,"Yağ oranı günlük alınması gereken yağ oranının üstündedir.")
+            print(p,"Doymuş Yağ oranı günlük alınması gereken yağ oranının altındadır.")
+            print(p,"Günlük tüketilmesi gereken karhonhidrat bakımından çok üstündedir")
+            print(p,"Günlük tüketilmesi gereken protein bakımından çok üstündedir.")
+            print(p,"Günlük tüketilmesi gereken lif bakımından yeterli.")
+            print(p,"Günlük tüketilmesi gereken tuz bakımından üstündedir")
+            print(p,"İçerisinde kolajen bulunmuyor.")
+            print(p,"İçerisinde trans yağ bulunmuyor.")
+            print(p,"İçerisinde palm yağ bulunuyor.")
+            print("Günlük alınması gereken şeker miktarından fazla.")
+        elif secim == '5':
+            p = 'Cipso'
+            print(p,"Yağ oranı günlük alınması gereken yağ oranının çok üstündedir")
+            print(p,"Doymuş Yağ oranı günlük alınması gereken yağ oranının üstündedir")
+            print(p,"Günlük tüketilmesi gereken karhonhidrat bakımından çok üstündedir")
+            print(p,"Günlük tüketilmesi gereken protein bakımından yeterli.")
+            print(p,"Günlük tüketilmesi gereken lif bakımından üstündedir")
+            print(p,"Günlük tüketilmesi gereken tuz bakımından çok üstündedir")
+            print(p,"İçerisinde kolajen bulunmuyor.")
+            print(p,"İçerisinde trans yağ bulunmuyor.")
+            print(p,"İçerisinde palm yağ bulunuyor.")
+            print(p,"Günlük alınması gereken şeker miktarından fazla.")
+        elif secim == '6':
+            p = 'Vegeat s'
+            print(p,"Yağ oranı günlük alınması gereken yağ oranının çok üstündedir")
+            print(p,"Doymuş Yağ oranı günlük alınması gereken yağ oranının altındadır")
+            print(p,"Günlük tüketilmesi gereken karhonhidrat bakımından çok üstündedir")
+            print(p,"Günlük tüketilmesi gereken protein bakımından çok üstündedir.")
+            print(p,"Günlük tüketilmesi gereken lif bakımından üstündedir")
+            print(p,"Günlük tüketilmesi gereken tuz bakımından çok üstündedir")
+            print(p,"İçerisinde kolajen bulunmuyor.")
+            print(p,"İçerisinde esen miktarda trans yağ bulunuyor.")
+            print(p,"İçerisinde palm yağ bulunuyor.")
+            print("Günlük alınması gereken şeker miktarından fazla.")
+        elif secim == '7':
+            p = 'Doritos'
+            print(p,"Yağ oranı günlük alınması gereken yağ oranının altındadır")
+            print(p,"Doymuş Yağ oranı günlük alınması gereken yağ oranının altındadır")
+            print(p,"Günlük tüketilmesi gereken karhonhidrat bakımından yeterli")
+            print(p,"Günlük tüketilmesi gereken protein bakımından yeterli.")
+            print(p,"Günlük tüketilmesi gereken lif bakımından yeterli.")
+            print(p,"Günlük tüketilmesi gereken tuz bakımından yeterli.")
+            print(p,"İçerisinde kolajen bulunmuyor.")
+            print(p,"İçerisinde trans yağ bulunmuyor.")
+            print(p,"İçerisinde palm yağ bulunmuyor.")
+            print("Günlük alınması gereken şeker miktarına uygun.")
+        elif secim == '8':
+            p = 'Çerezza'
+            print(p,"Yağ oranı günlük alınması gereken yağ oranının üstündedir")
+            print(p,"Doymuş Yağ oranı günlük alınması gereken yağ oranının altındadır")
+            print(p,"Günlük tüketilmesi gereken karhonhidrat bakımından çok üstündedir")
+            print(p,"Günlük tüketilmesi gereken protein bakımından yeterli.")
+            print(p,"Günlük tüketilmesi gereken lif bakımından üstündedir.")
+            print(p,"Günlük tüketilmesi gereken tuz bakımından üstündedir.")
+            print(p,"İçerisinde kolajen bulunmuyor.")
+            print(p,"İçerisinde trans yağ bulunuyor.")
+            print(p,"İçerisinde palm yağ bulunuyor.")
+            print("Günlük alınması gereken şeker miktarından yüksek.")
+        else:
+            print("Geçersiz değer girdiniz. Lütfen tekrar deneyiniz.")
+            
+    def KaloriTespit():
+        twosoru = input("Cinsiyetiniz (K/E):  ")
+        threesoru = int (input("Yaşınız: "))
+        foursoru = int(input("Kilonuz: "))
+        fivesoru = int(input("Boyunuz: "))
+        if twosoru == 'K' or twosoru == 'k':
+            son1 = 655 + 9.5 * foursoru + 1.8 * fivesoru - 4.7 * threesoru
+            sixsoru = input("Ne sıklıkla egzersiz yaparsınız ?(1= Çok az 2= Az 3= Orta 4= İyi 5=Çok İyi: ")
+            if sixsoru == '1':
+                son2 = son1 * 1.2
+                print(f"Günde ihtiyaç olduğunuz kalori miktarı: {son2}")
+            elif sixsoru == '2':
+                son2 = son1 * 1.375
+                print(f"Günde ihtiyaç olduğunuz kalori miktarı: {son2}")
+            elif sixsoru == '3':
+                son2 = son1 * 1.55
+                print(f"Günde ihtiyaç olduğunuz kalori miktarı: {son2}")
+            elif sixsoru == '4':
+                son2 = son1 * 1.725
+                print(f"Günde ihtiyaç olduğunuz kalori miktarı: {son2}")
+            elif sixsoru == '5':
+                son2 = son1 * 1.9
+                print(f"Günde ihtiyaç olduğunuz kalori miktarı: {son2}")
+            else:
+                print("Girdiğiniz değer hatalıdır.")
+        elif twosoru == 'E' or twosoru == 'e' :
+            son1 = 66 + 13.7 * foursoru + 5 * fivesoru - 6.8 * threesoru
+            sixsoru = input("Ne sıklıkla egzersiz yaparsınız ?(1= Çok az 2= Az 3= Orta 4= İyi 5=Çok İyi: ")
+            if sixsoru == '1':
+                son3 = son1 * 1.2
+                print(f"Günde ihtiyaç olduğunuz kalori miktarı: {son3}")
+            elif sixsoru == '2':
+                son3 = son1 * 1.375
+                print(f"Günde ihtiyaç olduğunuz kalori miktarı: {son3}")
+            elif sixsoru == '3':
+                son3 = son1 * 1.55
+                print(f"Günde ihtiyaç olduğunuz kalori miktarı: {son3}")
+            elif sixsoru == '4':
+                son3 = son1 * 1.725
+                print(f"Günde ihtiyaç olduğunuz kalori miktarı: {son3}")
+            elif sixsoru == '5':
+                son3 = son1 * 1.9
+                print(f"Günde ihtiyaç olduğunuz kalori miktarı: {son3}")
+            else:
+                print("Girdiğiniz değer hatalıdır.")
+        else:
+            print("girilen değer yanlış verilmiştir.")
+   
+onesoru = input("Programamı ne için kullanacaksınız: 1-) Kilo vermek 2-) Kilo almak(bakımda): ")
+
+if onesoru == '1':
+    sonuc = Kalori.KaloriTespit()
+    sonucson = Kalori.HangCip()
+elif onesoru == '2':
+    pass
+else:
+    print("Hatalı bir değer girdiniz. Lütfen tekrar deneyiniz.")
+
+
+
+
+# 
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
